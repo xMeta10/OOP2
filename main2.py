98a531a2b9c0ee134aa09914e33a6f3160e472ad
Meta <146641115+xMeta10@users.noreply.github.com>
2026-04-13 20:03:08 +0500

    add all proj

--- main2.py
+++ main2.py
@@ -419,3 +419,19 @@ class MainWindow(QMainWindow):
         event.accept()
 
 
+def main():
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+
+    # Установка более современного шрифта по умолчанию
+    font = QFont("Segoe UI", 10)
+    app.setFont(font)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
